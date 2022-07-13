8afa542d4a0c96892e0f9682ab8aba58e1b1adbc
KeplerBoyce <keplerboy@gmail.com>
2022-07-12 23:31:14 -0700

    header component

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <div className="flex gap-1 sticky top-0 py-2 px-6 border-b border-gray-400">
+            <span className="text-2xl font-bold">
+                Header
+            </span>
+        </div>
+    )
+}
