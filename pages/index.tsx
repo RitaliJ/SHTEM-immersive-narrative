bc3d64bf583046c344e099588c81eee4ac8f872f
KeplerBoyce <keplerboy@gmail.com>
2022-07-14 13:56:36 -0700

    fix header positioning

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,16 @@ import Header from '../components/Header'
 
 export default function Home() {
     return (
-        <div className="w-full">
+        <div className="w-screen">
             <Head>
                 <title>SHTEM Website</title>
                 <meta name="description" content="Shtem website" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            
+            <Header />
 
             <main className="container pt-12">
-                <Header />
 
                 <h1 className="text-7xl font-bold text-center">
                     Buy our things
