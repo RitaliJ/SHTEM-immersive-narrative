86625aeb9b520cadbff400051f68e0a11531a824
KeplerBoyce <keplerboy@gmail.com>
2022-07-14 12:30:53 -0700

    formatting

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,21 @@ import Head from 'next/head'
 import Header from '../components/Header'
 
 export default function Home() {
-  return (
-    <div className="w-full">
-      <Head>
-        <title>SHTEM Website</title>
-        <meta name="description" content="Shtem website" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className="w-full">
+            <Head>
+                <title>SHTEM Website</title>
+                <meta name="description" content="Shtem website" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main className="container pt-12">
-        <Header />
+            <main className="container pt-12">
+                <Header />
 
-        <h1 className="text-7xl font-bold text-center">
-          Buy our things
-        </h1>
-      </main>
-    </div>
-  )
+                <h1 className="text-7xl font-bold text-center">
+                    Buy our things
+                </h1>
+            </main>
+        </div>
+    )
 }
