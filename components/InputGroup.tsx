e76b78cf7da8250d63ed0950b1ad9d13b64e0cf9
KeplerBoyce <keplerboy@gmail.com>
2022-07-14 14:09:22 -0700

    increase input padding

--- components/InputGroup.tsx
+++ components/InputGroup.tsx
@@ -12,7 +12,7 @@ export default function InputGroup(props: {
             </p>
             <input
                 type="text"
-                className="border-2 border-gray-400 px-1 rounded-md"
+                className="border-2 border-gray-400 px-2 py-1 rounded-md"
                 placeholder={placeholder}
                 onChange={(e) => callback(e.target.value)}
             />
