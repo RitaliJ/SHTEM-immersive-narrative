0344df4459f5394fc0c8eaff558e130c8a1072c8
KeplerBoyce <keplerboy@gmail.com>
2022-07-15 13:20:18 -0700

    product previews on home page

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
 import Header from '../components/Header'
+import ProductPreview from '../components/ProductPreview';
 import { AccountType } from '../util/types';
 
 export default function Home() {
@@ -18,7 +19,7 @@ export default function Home() {
     });
 
     return (
-        <div className="w-screen">
+        <div className="bg-white">
             <Head>
                 <title>SHTEM Website</title>
                 <meta name="description" content="Shtem website" />
@@ -27,11 +28,68 @@ export default function Home() {
             
             <Header account={account} />
 
-            <main className="container pt-12">
-
-                <h1 className="text-7xl font-bold text-center">
+            <main className="container pt-12 pb-24">
+                <h1 className="text-7xl font-bold text-center mb-12">
                     Buy our things
                 </h1>
+                <div className="flex justify-center gap-4 flex-wrap">
+                    <ProductPreview
+                        imgSrc="https://www.ikea.com/us/en/images/products/micke-desk-black-brown__0735981_pe740299_s5.jpg"
+                        name="MICKE desk"
+                        desc="Top 10 desk of all time"
+                        price={39.99}
+                        id={0}
+                    />
+                    <ProductPreview
+                        imgSrc="https://www.ikea.com/us/en/images/products/micke-desk-black-brown__0735981_pe740299_s5.jpg"
+                        name="MICKE desk"
+                        desc="Top 10 desk of all time"
+                        price={39.99}
+                        id={0}
+                    />
+                    <ProductPreview
+                        imgSrc="https://www.ikea.com/us/en/images/products/micke-desk-black-brown__0735981_pe740299_s5.jpg"
+                        name="MICKE desk"
+                        desc="Top 10 desk of all time"
+                        price={39.99}
+                        id={0}
+                    />
+                    <ProductPreview
+                        imgSrc="https://www.ikea.com/us/en/images/products/micke-desk-black-brown__0735981_pe740299_s5.jpg"
+                        name="MICKE desk"
+                        desc="Top 10 desk of all time"
+                        price={39.99}
+                        id={0}
+                    />
+                    <ProductPreview
+                        imgSrc="https://www.ikea.com/us/en/images/products/micke-desk-black-brown__0735981_pe740299_s5.jpg"
+                        name="MICKE desk"
+                        desc="Top 10 desk of all time"
+                        price={39.99}
+                        id={0}
+                    />
+                    <ProductPreview
+                        imgSrc="https://www.ikea.com/us/en/images/products/micke-desk-black-brown__0735981_pe740299_s5.jpg"
+                        name="MICKE desk"
+                        desc="Top 10 desk of all time"
+                        price={39.99}
+                        id={0}
+                    />
+                    <ProductPreview
+                        imgSrc="https://www.ikea.com/us/en/images/products/micke-desk-black-brown__0735981_pe740299_s5.jpg"
+                        name="MICKE desk"
+                        desc="Top 10 desk of all time"
+                        price={39.99}
+                        id={0}
+                    />
+                    <ProductPreview
+                        imgSrc="https://www.ikea.com/us/en/images/products/micke-desk-black-brown__0735981_pe740299_s5.jpg"
+                        name="MICKE desk"
+                        desc="Top 10 desk of all time"
+                        price={39.99}
+                        id={0}
+                    />
+                </div>
             </main>
         </div>
     )
