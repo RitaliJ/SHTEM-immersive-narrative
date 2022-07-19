a16d32c3fba7dc2084beaf28917c2c205c1f8604
KeplerBoyce <keplerboy@gmail.com>
2022-07-19 15:10:17 -0700

    minor styling

--- components/ProductPreview.tsx
+++ components/ProductPreview.tsx
@@ -8,7 +8,7 @@ export default function ProductPreview(props: {product: ProductType}) {
     return (
         <Link href={"/products/" + product.id}>
             <div className="flex flex-col gap-1 w-96 bg-gray-200 hover:bg-gray-300 hover:cursor-pointer
-                hover:shadow-xl duration-300 rounded-lg p-4">
+                shadow-md hover:shadow-xl duration-300 rounded-lg p-4">
                 <img src={product.imgSrc} alt={product.name} />
                 <div className="flex mt-1 text-lg gap-2">
                     <p className="font-bold grow line-clamp-2">
