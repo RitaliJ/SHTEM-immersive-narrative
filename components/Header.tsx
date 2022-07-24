4540d4adda69cc3b4160d4c016e0c64405efafca
KeplerBoyce <keplerboy@gmail.com>
2022-07-24 16:57:47 -0700

    change cart svg

--- components/Header.tsx
+++ components/Header.tsx
@@ -128,8 +128,8 @@ export default function Header(props: {
                 onClick={() => {router.pathname !== "/checkout" && setIsOpen(true)}}
                 className="flex items-center hover:bg-gray-200 duration-150 p-2 rounded-lg"
             >
-                <svg className="w-8 h-8" viewBox="0 0 24 24">
-                    <path d="M17,18C15.89,18 15,18.89 15,20A2,2 0 0,0 17,22A2,2 0 0,0 19,20C19,18.89 18.1,18 17,18M1,2V4H3L6.6,11.59L5.24,14.04C5.09,14.32 5,14.65 5,15A2,2 0 0,0 7,17H19V15H7.42A0.25,0.25 0 0,1 7.17,14.75C7.17,14.7 7.18,14.66 7.2,14.63L8.1,13H15.55C16.3,13 16.96,12.58 17.3,11.97L20.88,5.5C20.95,5.34 21,5.17 21,5A1,1 0 0,0 20,4H5.21L4.27,2M7,18C5.89,18 5,18.89 5,20A2,2 0 0,0 7,22A2,2 0 0,0 9,20C9,18.89 8.1,18 7,18Z"/>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                 </svg>
                 {router.pathname !== "/checkout" && account.items && account.items[0] &&
                     <p className="bg-red-500 font-bold text-white rounded-full px-2">
