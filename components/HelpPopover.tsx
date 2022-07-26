d8880bd0ba58fc0a47f314db7e6d0ef6c254e827
RitaliJ <ritaliaarav@gmail.com>
2022-07-25 18:25:19 -0700

    clear red help nottif after opened

--- components/HelpPopover.tsx
+++ components/HelpPopover.tsx
@@ -4,17 +4,18 @@ import { ReactNode } from 'react';
 //modal for help popup
 export default function HelpPopover(props: {html: ReactNode, outerHtml: ReactNode}) {
     const {html, outerHtml} = props;
+    let opened = false;
 
     return (
         <Popover as="div" className="relative">
             {({open}) => (
                 <>
-                    <Popover.Button className="relative outline-none flex items-center hover:bg-gray-200 duration-150 p-2 rounded-lg">
+                    <Popover.Button className="relative outline-none flex items-center hover:bg-gray-200 duration-150 p-2 rounded-lg" onClick={()=> {opened=true}}>
                         <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
                         <div className={"duration-300 absolute top-2 right-1 w-4 h-4 rounded-full bg-red-500 "
-                            + (open ? "bg-opacity-0" : "bg-opacity-100")} />
+                            + ((open || opened) ? "bg-opacity-0" : "bg-opacity-100")} />
                     </Popover.Button>
                     <Transition
                         as="div"
