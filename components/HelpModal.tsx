0c2712e1127f69e4f6c02ff59403418666b99d83
KeplerBoyce <keplerboy@gmail.com>
2022-07-26 10:22:17 -0700

    streamline carty intro

--- components/HelpModal.tsx
+++ components/HelpModal.tsx
@@ -6,34 +6,30 @@ import DropdownMenu from "./DropdownMenu";
 export default function HelpModal(props: {isOpen: boolean, setIsOpen: (value: boolean) => void, account: AccountType}) {
     const {isOpen, setIsOpen, account} = props;
     const [page, setPage] = useState(0);
-    const [canContinue, setCanContinue] = useState(true);
     const [blur, setBlur] = useState("lg");
-    const [target, setTarget] = useState("");
-    const targetItems = ["Accordion Outfit", "Avocado", "Phoenix"];
     
     const pages: ReactNode[] = [
         <p key={0}>
             Hello {account.firstName}! Thank you for signing up for our website;
             we{"'"}re so happy to have you join us! My name is Carty, your friendly
-            personal shopping guide. I{"'"}m here to show you around our store and
-            help you with any questions that may pop up!
+            personal shopping guide.
         </p>,
         <p key={1}>
+            I{"'"}m here to show you around our store and help you with any questions that may pop up!
+        </p>,
+        <p key={2}>
             Welcome to the homepage of our store, which only contains the trendiest
             clothing and accessories!
         </p>,
-        <div key={2}>
-            <p>
-                Our store is very special as we don{"'"}t take payment through physical currency,
-                but rather through our own personal currency of tokens. Luckily for you, you
-                get to start off with 100 tokens already! Is there something in particular
-                you{"'"}re looking to buy?
-            </p>
-            <div className="w-full">
-                <DropdownMenu label="Choose an item" callback={x => setTarget(x)} options={targetItems} />
-            </div>
-        </div>,
-        <div key={3} className="flex gap-1 flex-wrap items-center">
+        <p key={3}>
+            Our store is very special as we don{"'"}t take payment through physical currency,
+            but rather through our own personal currency of tokens.
+        </p>,
+        <p key={4}>
+            Luckily for you, you get to start off with 100 tokens already! Have a look around
+            the store and see what interests you.
+        </p>,
+        <div key={5} className="flex gap-1 flex-wrap items-center">
             <p>That concludes my introduction. if you ever need any help, you can find me by pressing the</p>
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -42,22 +38,11 @@ export default function HelpModal(props: {isOpen: boolean, setIsOpen: (value: bo
         </div>,
     ];
 
-    //allow continue only if target product is chosen
-    useEffect(() => {
-        if (page === 2) {
-            if (target) setCanContinue(true);
-            else setCanContinue(false);
-        }
-    }, [target]);
-
     //function for advancing to next page and handling any other effects
     const advance = () => {
-        if (canContinue) {
-            if (page === 0) setBlur("");
-            if (page === 1) setCanContinue(false);
-            if (page === pages.length - 1) setIsOpen(false);
-            else setPage(page + 1);
-        }
+        if (page === 1) setBlur("");
+        if (page === pages.length - 1) setIsOpen(false);
+        else setPage(page + 1);
     }
 
     return (
@@ -72,16 +57,10 @@ export default function HelpModal(props: {isOpen: boolean, setIsOpen: (value: bo
                     {pages[page]}
                     <button
                         onClick={() => advance()}
-                        className={"px-3 py-1 text-lg rounded-lg mt-2 shadow-lg duration-150 "
-                            + (canContinue ? "bg-blue-500 text-white" : "bg-gray-200 text-gray-400")}
+                        className="px-3 py-1 text-lg rounded-lg mt-2 shadow-lg duration-150 bg-blue-500 text-white"
                     >
                         {page === pages.length - 1 ? "Finish intro" : "Next"}
                     </button>
-                    {!canContinue &&
-                        <p className="text-red-500 italic">
-                            * Please fill out all required fields
-                        </p>
-                    }
                 </div>
             </div>
         </CenteredModal>
