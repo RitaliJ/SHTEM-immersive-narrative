b31d1a1fab39170539444c3cd5b2e7a2136f2346
KeplerBoyce <keplerboy@gmail.com>
2022-07-29 10:42:37 -0700

    fix inline svgs

--- components/HelpModal.tsx
+++ components/HelpModal.tsx
@@ -29,12 +29,20 @@ export default function HelpModal(props: {isOpen: boolean, setIsOpen: (value: bo
             Luckily for you, you get to start off with 100 Heartbeats already! Have a look around
             the store and see what interests you.
         </p>,
-        <div key={5} className="flex gap-1 flex-wrap items-center">
-            <p>That concludes my introduction. if you ever need any help, you can find me by pressing the</p>
+        <div key={5} className="flex gap-x-1 flex-wrap items-center h-min">
+            {"That concludes my introduction. if you ever need any help, you can find me by pressing the".split(" ").map((w, i) =>
+                <p key={i}>
+                    {w}
+                </p>
+            )}
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            <p>icon in the header.</p>
+            {"icon in the header.".split(" ").map((w, i) =>
+                <p key={i}>
+                    {w}
+                </p>
+            )}
         </div>,
     ];
 
