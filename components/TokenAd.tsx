d4486d162f0a446bde55b6355385137be2ec32d4
KeplerBoyce <keplerboy@gmail.com>
2022-08-01 17:05:12 -0700

    basic instructions for token ad

--- components/TokenAd.tsx
+++ components/TokenAd.tsx
@@ -47,42 +47,43 @@ export default function TokenAd(props: {
 
     return (
         <CenteredModal isOpen={isOpen} setIsOpen={setIsOpen}>
-            <div className="relative flex flex-col gap-2 p-4 items-center bg-white w-min whitespace-nowrap rounded-lg oveflow-hidden">
+            <div className="relative flex flex-col gap-2 p-4 items-center bg-white w-min rounded-lg whitespace-nowrap oveflow-hidden">
                 <button
                     onClick={() => setIsOpen(false)}
                     className="absolute top-2 right-4 text-5xl"
                 >
                     ×
                 </button>
-                {cameraAllowed ?
-                    <>
-                        <img src = "https://cdn.discordapp.com/attachments/996489060275208295/1003691578558267524/B0D3893A-CEC3-4463-A2C6-38531F18C1B4.png"></img>
-                        <div className="flex gap-1 text-2xl">
-                            <p>Heartrate:</p>
-                            <p className="font-bold">
-                                {heartrate} BPM
-                            </p>
-                        </div>
-                        <p className="text-xl">
-                            Current reward: {reward} Heartbeats
+                {cameraAllowed ? <>
+                    <img src = "https://cdn.discordapp.com/attachments/996489060275208295/1003691578558267524/B0D3893A-CEC3-4463-A2C6-38531F18C1B4.png"></img>
+                    <div className="flex gap-1 text-2xl">
+                        <p>Heartrate:</p>
+                        <p className="font-bold">
+                            {heartrate} BPM
                         </p>
-                        <p className="italic">
-                            You can close this popup at any time to receive your Heartbeats
-                        </p>
-                    </> : <>
-                        <p className="text-2xl font-bold">
-                            Authorize camera
-                        </p>
-                        <img src = "https://cdn.discordapp.com/attachments/996489060275208295/1003694136941084832/BA6EE846-2205-4343-AA9A-8DFDBED21E0A.jpg"></img>
-
-                        <button
-                            onClick={() => setCameraAllowed(true)}
-                            className="bg-green-600 text-white rounded-lg px-24 py-3"
-                        >
-                            Allow
-                        </button>
-                    </>
-                }
+                    </div>
+                    <p className="text-xl">
+                        Current reward: {reward} Heartbeats
+                    </p>
+                    <p className="italic">
+                        You can close this popup at any time to receive your Heartbeats
+                    </p>
+                </> : <>
+                    <p className="text-2xl font-bold px-12 whitespace-nowrap">
+                        Authorize camera
+                    </p>
+                    <img src = "https://cdn.discordapp.com/attachments/996489060275208295/1003694136941084832/BA6EE846-2205-4343-AA9A-8DFDBED21E0A.jpg"></img>
+                    <p className="text-lg break-words text-center whitespace-normal italic leading-6">
+                        We need to make sure that you're looking at the ad to give you your reward. You will
+                        earn heartbeats based on your heartrate while viewing the ad.
+                    </p>
+                    <button
+                        onClick={() => setCameraAllowed(true)}
+                        className="bg-green-600 text-white rounded-lg px-24 py-3"
+                    >
+                        Allow
+                    </button>
+                </>}
             </div>
         </CenteredModal>
     )
